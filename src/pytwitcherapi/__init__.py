b2674712a9ea6565998497baef0fa5b4fe2b4104
David Zuber <zuber.david@gmx.de>
2015-04-10 15:24:02 +0200

    Get rid of insecure platform warnings by using pyopenssl

--- src/pytwitcherapi/__init__.py
+++ src/pytwitcherapi/__init__.py
@@ -1,3 +1,9 @@
 __author__ = 'David Zuber'
 __email__ = 'zuber.david@gmx.de'
 __version__ = '0.1.4'
+
+
+# this will get rid of some insecure platform warnings
+# and enable SNI-support
+import urllib3.contrib.pyopenssl
+urllib3.contrib.pyopenssl.inject_into_urllib3()
