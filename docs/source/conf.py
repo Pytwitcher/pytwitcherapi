5f6baa9ee8dd648aa18fc00458945a1d8c1f477c
David Zuber <zuber.david@gmx.de>
2015-05-30 19:01:36 +0200

    Bump version: 0.7.1 → 0.7.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ master_doc = 'index'
 project = u'pytwitcherapi'
 copyright = u'2015, David Zuber'
 
-version = '0.7.1'
-release = '0.7.1'
+version = '0.7.2'
+release = '0.7.2'
 
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
