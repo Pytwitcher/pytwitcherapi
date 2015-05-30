bdb88dddc466df85547967a9bff9438f11cbfa97
David Zuber <zuber.david@gmx.de>
2015-05-30 19:01:25 +0200

    Bump version of conf to old version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,8 @@ master_doc = 'index'
 project = u'pytwitcherapi'
 copyright = u'2015, David Zuber'
 
-version = '0.6.0'
-release = '0.6.0'
+version = '0.7.1'
+release = '0.7.1'
 
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
